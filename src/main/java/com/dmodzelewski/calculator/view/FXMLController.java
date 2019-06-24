484edc4b110187dcf8b9b41adab836285e6b9a69
carrpenoctem <dn.mski@gmail.com>
2019-06-24 22:29:07 +0200

    Set proper methods for preparing calculations, added button for final calculation, added list of action buttons and first number variable

--- src/main/java/com/dmodzelewski/calculator/view/FXMLController.java
+++ src/main/java/com/dmodzelewski/calculator/view/FXMLController.java
@@ -2,8 +2,11 @@ package com.dmodzelewski.calculator.view;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -12,24 +15,43 @@ import javafx.scene.layout.*;
 
 public class FXMLController implements Initializable {
 
+    // Logger for logging errors
+    private static final Logger classLogger = Logger.getLogger(FXMLController.class.getCanonicalName());
+
+    // TextField where our numbers appear
     @FXML
-    private TextField inputField;
+    private TextField inputField = new TextField();
+    // Button container for simple calculator
     @FXML
     private GridPane simpleGrid;
+    // Button container for advanced calc
     @FXML
     private GridPane advancedGrid;
+    // Button container for nerdy calc
     @FXML
     private GridPane nerdGrid;
+    // Operation buttons
+    @FXML
+    private Button addBut, subBut, multiBut, divBut;
+
+    // list of all action buttons since I was too lazy to set id for panes 
+    // they are stored in
+    private List<Button> actionButtons = new ArrayList<>();
+    // first number we use in our equation
+    private double firstNumber;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        // populating actionButtons list
+        actionButtons.addAll(Arrays.asList(addBut, subBut, multiBut, divBut));
 
         // getting all nodes from grids
         List<Node> buttonsList_Node = new ArrayList<>();
         buttonsList_Node.addAll(simpleGrid.getChildren());
         buttonsList_Node.addAll(advancedGrid.getChildren());
         buttonsList_Node.addAll(nerdGrid.getChildren());
-        
+
         // casting nodes into buttons
         List<Button> buttonList = new ArrayList<>();
         for (Node b : buttonsList_Node) {
@@ -37,19 +59,88 @@ public class FXMLController implements Initializable {
                 buttonList.add((Button) b);
             }
         }
-        
+
         // setting action on buttons depending on their value
         for (Button b : buttonList) {
-            if ("piBut".equals(b.getId())||"piBut1".equals(b.getId())||"piBut2".equals(b.getId())) {
+            if ("piBut".equals(b.getId()) || "piBut1".equals(b.getId()) || "piBut2".equals(b.getId())) {
                 b.setOnAction((event) -> {
-                    System.out.println(Math.PI);
+                    inputField.setText(Math.PI + "");
                 });
             } else {
                 b.setOnAction((event) -> {
-                    System.out.println(b.getText());
+                    setInputField(b);
                 });
             }
         }
+    }
 
+    // useful method for clearing our textfield togglebutton and stored variable
+    @FXML
+    private void clearInput() {
+        inputField.clear();
+        firstNumber = 0;
+        for (Button b : actionButtons) {
+            b.setDisable(false);
+        }
+    }
+
+    // methods for operations
+    // type of operation will be based on which button is disabled
+    @FXML
+    private void addNumber() {
+        if (!inputField.getText().isEmpty()) {
+            prepareFirstNumber();
+            addBut.setDisable(true);
+        }
+    }
+    @FXML
+    private void subtractNumber() {
+        if (!inputField.getText().isEmpty()) {
+            prepareFirstNumber();
+            subBut.setDisable(true);
+        }
+    }
+    @FXML
+    private void multiplyNumber() {
+        if (!inputField.getText().isEmpty()) {
+            prepareFirstNumber();
+            multiBut.setDisable(true);
+        }
+    }
+    @FXML
+    private void divideNumber() {
+        if (!inputField.getText().isEmpty()) {
+            prepareFirstNumber();
+            divBut.setDisable(true);
+        }
+    }
+    
+    // final method for computing output
+    @FXML
+    private void countNumber(){
+        
+    }
+
+    // method for setting value for our inputField
+    private void setInputField(Button b) {
+
+        if (".".equals(b.getText()) && inputField.getText().contains(".")) {
+            classLogger.log(Level.WARNING, ". is already in there");
+        } else if (!".".equals(b.getText()) && "0".equals(inputField.getText())) {
+            classLogger.log(Level.WARNING, "0 can be in front for decimals only");
+            inputField.setText(b.getText());
+        } else if (".".equals(b.getText()) && inputField.getText().length() < 1) {
+            classLogger.log(Level.WARNING, "number cannot start with .");
+        } else {
+            inputField.setText(inputField.getText().concat(b.getText()));
+        }
+    }
+
+    private void prepareFirstNumber() {
+        firstNumber = Double.parseDouble(inputField.getText());
+        inputField.clear();
+        for (Button b : actionButtons) {
+            b.setDisable(false);
+        }
     }
 }
