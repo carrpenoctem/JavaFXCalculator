d505d250a742375cc165dcfe0a6bdc6bb9bbb2dc
carrpenoctem <dn.mski@gmail.com>
2019-06-24 23:28:40 +0200

    Simple calculator seems to be working

--- src/main/java/com/dmodzelewski/calculator/view/FXMLController.java
+++ src/main/java/com/dmodzelewski/calculator/view/FXMLController.java
@@ -27,6 +27,9 @@ public class FXMLController implements Initializable {
     // Button containers for simple,adv and nerd operations
     @FXML
     private VBox simpleVbox, advancedVbox, nerdVbox;
+    // Our calculate button
+    @FXML
+    private Button calculateBut;
 
     // list of all digit buttons
     private List<Button> buttonList = new ArrayList<>();
@@ -36,9 +39,14 @@ public class FXMLController implements Initializable {
     private double firstNumber;
     // second number we use in our equation
     private double secondNumber;
+    // default style of button (for calculate button listener)
+    private String defaultStyle;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        // getting default style of our calc button
+        defaultStyle = calculateBut.getStyle();
 
         // getting all nodes from grids
         List<Node> buttonsList_Node = new ArrayList<>();
@@ -85,15 +93,26 @@ public class FXMLController implements Initializable {
             b.setOnAction((event) -> {
                 prepareFirstNumber(b);
                 System.out.println(firstNumber);
+                System.out.println(secondNumber);
             });
         }
+
+        // a little visual tip when we can calculate value
+        inputField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (firstNumber != 0 && !"".equals(inputField.getText())) {
+                calculateBut.setStyle("-fx-background-color:LIME");
+            } else {
+                calculateBut.setStyle(defaultStyle);
+            }
+        });
     }
 
-    // useful method for clearing our textfield togglebutton and stored variable
+    // useful method for clearing our textfield togglebutton and stored variables
     @FXML
     private void clearInput() {
         inputField.clear();
         firstNumber = 0;
+        secondNumber = 0;
         for (Button b : actionButtons) {
             b.setDisable(false);
         }
@@ -102,7 +121,15 @@ public class FXMLController implements Initializable {
     // final method for computing output
     @FXML
     private void countNumber() {
-        secondNumber = Double.parseDouble(inputField.getText());
+        if (!inputField.getText().isEmpty()) {
+            secondNumber = Double.parseDouble(inputField.getText());
+            for (Button b : actionButtons) {
+                if (b.isDisabled()) {
+                    detectOperation(b);
+                }
+            }
+        }
+
     }
 
     // method for setting value for our inputField
@@ -131,5 +158,37 @@ public class FXMLController implements Initializable {
             }
             b.setDisable(true);
         }
+    }
+
+    // method to determine what mathematical operation should be used
+    private void detectOperation(Button b) {
+        String buttonId = b.getId();
+        double result = 0;
+
+        switch (buttonId) {
+            case "addBut":
+                result = firstNumber + secondNumber;
+                break;
+            case "subBut":
+                result = firstNumber - secondNumber;
+                break;
+            case "multiBut":
+                result = firstNumber * secondNumber;
+                break;
+            case "divBut":
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    classLogger.log(Level.SEVERE, "divided by 0");
+                }
+                break;
+        }
+        if (buttonId.equals("divBut") && secondNumber == 0) {
+            inputField.setText("DIVISION BY ZERO");
+            calculateBut.setStyle("-fx-background-color:RED");
+        } else {
+            inputField.setText(result + "");
+        }
+
     }
 }
